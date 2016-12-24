e46b3783cc2de078a349c5fb73eec730d4ef1ba4
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-12-24 18:39:51 +0100

    some JS

--- src/c/nim.c
+++ src/c/nim.c
@@ -54,6 +54,20 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
 }
 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
 
 static void init() {
   // Create main Window element and assign to pointer
@@ -73,6 +87,17 @@ static void init() {
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+
+  // Open AppMessage
+  const int inbox_size = 128;
+  const int outbox_size = 128;
+  app_message_open(inbox_size, outbox_size);
 }
 
 static void deinit() {
