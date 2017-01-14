013361f60b1dafa37ee84c9d6fc4af1b4e791beb
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-01-14 19:31:53 +0100

    hardcode GET for no reasons

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,23 +1,23 @@
-var xhrRequest = function (url, type, callback) {
+var xhrRequest = function (url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () { callback(this.responseText); };
-  xhr.open(type, url);
+  xhr.open('GET', url);
   xhr.send();
 };
 
 function locationSuccess(pos) {
   var url = 'https://saurel.me/pebble/' + pos.coords.latitude + '/' + pos.coords.longitude;
-  xhrRequest(url, 'GET', function(responseText) { Pebble.sendAppMessage(JSON.parse(responseText)); });
+  xhrRequest(url, function(responseText) { Pebble.sendAppMessage(JSON.parse(responseText)); });
 }
 
 function locationError(err) {
   var url = 'https://saurel.me/pebble/';
-  xhrRequest(url, 'GET', function(responseText) { Pebble.sendAppMessage(JSON.parse(responseText)); });
+  xhrRequest(url, function(responseText) { Pebble.sendAppMessage(JSON.parse(responseText)); });
 }
 
-function getWeather() {
+function getLocation() {
   navigator.geolocation.getCurrentPosition( locationSuccess, locationError, {timeout: 15000, maximumAge: 60000});
 }
 
-Pebble.addEventListener('ready', function(e) { getWeather(); });
-Pebble.addEventListener('appmessage', function(e) { getWeather(); });
+Pebble.addEventListener('ready', function(e) { getLocation(); });
+Pebble.addEventListener('appmessage', function(e) { getLocation(); });
